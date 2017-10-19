8c4e7c867fb76f612ed24ee47067cd719a4683e1
Yiheng <development.henryquan@gmail.com>
2017-10-19 22:10:16 +1100

    Removing Native-Base
    I think I will write the major components I need and I dont think native-base is easy for me to use.

--- App.js
+++ App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
-import { Container, Header, Left, Body, Right, Button, Icon, Title } from 'native-base';
+import { View, Text } from 'react-native';
 
 export default class App extends Component {
   render() {
     return (
-      <Container>
-        <Header searchBar rounded>
-          <Body>
-            <Title>GoGoAnime</Title>
-          </Body>
-        </Header>
-      </Container>
+      <View>
+        <Text>Hello World</Text>
+      </View>
     );
   }
 }
