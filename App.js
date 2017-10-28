ccb200a9a85499e5187dd1eb761a661a260c3488
Yiheng <development.henryquan@gmail.com>
2017-10-28 14:31:53 +1100

    Adding screens
    New frameworks are added
    Adding all necessary screens

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
 export default class App extends Component {
+  static navigationOptions = {
+    title: 'GoGoAnime',
+  }
+
   render() {
     return (
       <View>
@@ -10,3 +15,4 @@ export default class App extends Component {
     );
   }
 }
+
