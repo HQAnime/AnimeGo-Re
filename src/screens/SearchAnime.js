80d7c9846a33691a385083b9a09c67e512e13b9f
Yiheng <development.henryquan@gmail.com>
2017-11-06 22:12:36 +1100

    Style change for TextInput IOS
    Android has its own border, I dont have to add it myself whereas IOS doesn't

--- src/screens/SearchAnime.js
+++ src/screens/SearchAnime.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
-import { View, TextInput } from 'react-native';
+import { View, TextInput, Platform } from 'react-native';
 
 class SearchAnime extends Component {
   render() {
     return (
       <View>
         <TextInput
-          style={{height: 40, borderColor: 'gray'}}
+          style={styles.inputStyle}
           onChangeText={(text) => this.setState({text})}
         />
       </View>
     );
+  }
+}
+
+const styles = {
+  inputStyle: {
+    height: 36, 
+    borderColor: 'gray',
+    borderBottomWidth: (Platform.OS === 'ios') ? 1 : 0,
   }
 }
 
