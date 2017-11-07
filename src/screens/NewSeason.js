293cb7507fcc3a558f4ee41396cc94de3674fdc1
Yiheng <development.henryquan@gmail.com>
2017-11-07 20:53:40 +1100

    Adding Constant file and trying to parse data
    Constant file is added for future use
    Trying to get website source code and it is working

--- src/screens/NewSeason.js
+++ src/screens/NewSeason.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { GoGoAnime } from '../Constant';
 
 class NewSeason extends Component {
+  componentWillMount() {
+    // Loading data here
+    fetch(GoGoAnime.NewRelease)
+    .then((html) => html.text())
+    .then((htmlText) => {
+      console.log(htmlText);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
+
   render() {
     return (
       <View>
