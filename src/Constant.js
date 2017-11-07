03cd739ef08566cccd1acbc2f947593577dddcb7
Yiheng <development.henryquan@gmail.com>
2017-11-07 20:53:40 +1100

    Adding Constant file and trying to parse data
    Constant file is added for future use
    Trying to get website source code and it is working

---
+++ src/Constant.js
@@ -0,0 +1,7 @@
+export const GoGoAnime = {
+  // Some constant link, easy to change in the future
+  NewRelease: 'https://ww3.gogoanime.io/?page=1',
+  NewSeason: 'https://ww3.gogoanime.io/new-season.html',
+  Movie: 'https://ww3.gogoanime.io/anime-movies.html',
+  Genre: 'https://ww3.gogoanime.io/genre/',
+}
