ce33d7383de15003febc19a7bcd7ca065b11c8c5
Yiheng <development.henryquan@gmail.com>
2017-11-07 21:33:52 +1100

    Adding fast-html-parser
    Getting lastest anime for gogoanime
    Infinity scrolling will be the next challenge

--- src/screens/RecentRelease.js
+++ src/screens/RecentRelease.js
@@ -1,13 +1,54 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView, View, Text, ActivityIndicator } from 'react-native';
+import { GoGoAnime } from '../Constant';
 
 class RecentRelease extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: ''
+    };
+  }
+
+  componentWillMount() {
+    // Loading data here
+    fetch(GoGoAnime.NewRelease)
+    .then((html) => html.text())
+    .then((htmlText) => {
+      var HTMLParser = require('fast-html-parser');
+      
+      var root = HTMLParser.parse(htmlText);
+      var episodes = root.querySelector('.items').rawText;
+      this.setState({
+        data: episodes
+      })
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
+
   render() {
+    /* A loading indictor */
+    if (this.state.data == '') {
+      return (
+        <View>
+          <ActivityIndicator style={styles.loadingStyle} size='large'/>
+        </View>
+      )
+    }
+
     return (
-      <View>
-        <Text>RecentRelease</Text>
-      </View>
+      <ScrollView>
+        <Text>{this.state.data}</Text>
+      </ScrollView>
     );
+  }
+}
+
+const styles = {
+  loadingStyle: {
+    padding: 10,
   }
 }
 
