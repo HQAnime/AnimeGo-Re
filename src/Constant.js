a4dd8e8caac1d77104fa549eb74a88d552175017
Yiheng <development.henryquan@gmail.com>
2017-11-08 21:22:32 +1100

    New Release has been complated
    It looks great on iPad and Oneplus. More styling will be added, such as a cardview... It will also have different numColumns for different devices.

--- src/Constant.js
+++ src/Constant.js
@@ -1,7 +1,7 @@
 export const GoGoAnime = {
   // Some constant link, easy to change in the future
   MainURL: 'https://ww3.gogoanime.io',
-  NewRelease: 'https://ww3.gogoanime.io/?page=1',
+  NewRelease: 'https://ww3.gogoanime.io/page-recent-release.html?page=',
   NewSeason: 'https://ww3.gogoanime.io/new-season.html',
   Movie: 'https://ww3.gogoanime.io/anime-movies.html',
   Genre: 'https://ww3.gogoanime.io/genre',
--- src/screens/RecentRelease.js
+++ src/screens/RecentRelease.js
@@ -1,41 +1,22 @@
 import React, { Component } from 'react';
-import { ScrollView, View, Text, ActivityIndicator } from 'react-native';
+import { Linking, TouchableOpacity, Image, FlatList, View, Text, ActivityIndicator } from 'react-native';
 import { GoGoAnime } from '../Constant';
 
 class RecentRelease extends Component {
+
+  keyExtractor = (item, index) => item.id;
+
   constructor(props) {
     super(props);
     this.state = {
-      data: ''
+      data: [],
+      page: 1,
+      hasMorePage: true
     };
   }
 
   componentWillMount() {
-    // Loading data here
-    fetch(GoGoAnime.NewRelease)
-    .then((html) => html.text())
-    .then((htmlText) => {
-      var HTMLParser = require('fast-html-parser');
-      
-      var root = HTMLParser.parse(htmlText);
-      var episodes = root.querySelector('.items');
-      var newDataString = '';
-      for (var i = 0; i < episodes.childNodes.length; i++) {
-        var anime = episodes.childNodes[i];
-        // Somehow, next line is parsed as well
-        if (anime.isWhitespace) continue;
-        var animeLink = GoGoAnime.MainURL + anime.querySelector('.img').childNodes[1].attributes.href;
-        var animeName = anime.querySelector('.name').text;
-        var animeEpisode = anime.querySelector('.episode').text;
-        newDataString += animeName + '\n' + animeEpisode + '\n' + animeLink + '\n\n';
-      }
-      this.setState({
-        data:newDataString
-      })
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+    this.loadAnime();
   }
 
   render() {
@@ -49,10 +30,66 @@ class RecentRelease extends Component {
     }
 
     return (
-      <ScrollView>
-        <Text>{this.state.data}</Text>
-      </ScrollView>
+      <FlatList
+        data={this.state.data} keyExtractor={item => item.name}
+        renderItem={({item}) => 
+        <View style={{padding: 10}}>
+          <TouchableOpacity onPress={() => Linking.openURL(item.link).catch(err => console.error('An error occurred', err))}>
+            <Text>{item.name}</Text>
+            <Text>{item.episode}</Text>
+            <Image source={{uri: item.thumbnail}} style={{width: 225, height: 326}} />
+          </TouchableOpacity>
+        </View>
+        } onEndReached={this.loadAnime} onEndReachedThreshold={0}
+        contentContainerStyle={{alignItems: 'center'}} ListFooterComponent={
+          <ActivityIndicator style={styles.loadingStyle} size='large'/>
+        }/>
     );
+  }
+
+  loadAnime = () => {
+    if (!this.state.hasMorePage) return;
+    // Loading data here
+    console.log(GoGoAnime.NewRelease + this.state.page);
+    fetch(GoGoAnime.NewRelease + this.state.page)
+    .then((html) => html.text())
+    .then((htmlText) => {
+      var HTMLParser = require('fast-html-parser');
+      
+      var root = HTMLParser.parse(htmlText);
+      var items = root.querySelector('.items').childNodes;
+      var animeData = this.state.data;
+
+      // In case last page is reached
+      var length = items.length;
+      if (length == 0) {
+        this.setState({
+          hasMorePage: false
+        })
+        return;
+      } 
+
+      for (var i = 0; i < length; i++) {
+        var anime = items[i];
+        // Somehow, next line is parsed as well
+        if (anime.isWhitespace) continue;
+
+        var animeImage = anime.querySelector('.img');
+        var animeLink = GoGoAnime.MainURL + animeImage.childNodes[1].attributes.href;
+        var animeName = anime.querySelector('.name').text;
+        var animeEpisode = anime.querySelector('.episode').text;
+        var animeThumbnail = animeImage.childNodes[1].childNodes[1].attributes.src;
+        animeData.push({name: animeName, episode: animeEpisode, link: animeLink, thumbnail: animeThumbnail});
+      }
+      this.setState({
+        data: animeData,
+        // Preparing for loading for data
+        page: this.state.page + 1
+      })
+    })
+    .catch((error) => {
+      console.error(error);
+    });
   }
 }
 
