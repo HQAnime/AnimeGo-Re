c484600e7abeb2a9d95a84f9d27ffda38d2a49f3
Yiheng <development.henryquan@gmail.com>
2017-11-09 21:49:32 +1100

    Movie and New Season
    Just one line of code.
    Creating a genre list.
    There seems to be some issue with infinite scroll for new season.

--- src/Constant.js
+++ src/Constant.js
@@ -2,7 +2,7 @@ export const GoGoAnime = {
   // Some constant link, easy to change in the future
   MainURL: 'https://ww3.gogoanime.io',
   NewRelease: 'https://ww3.gogoanime.io/page-recent-release.html?page=',
-  NewSeason: 'https://ww3.gogoanime.io/new-season.html',
-  Movie: 'https://ww3.gogoanime.io/anime-movies.html',
-  Genre: 'https://ww3.gogoanime.io/genre',
+  NewSeason: 'https://ww3.gogoanime.io/new-season.html?page=',
+  Movie: 'https://ww3.gogoanime.io/anime-movies.html?page=',
+  Genre: 'https://ww3.gogoanime.io/genre/',
 }
--- src/core/AnimeLoader.js
+++ src/core/AnimeLoader.js
@@ -23,18 +23,20 @@ export default class AnimeLoader {
         // In case last page is reached
         var length = items.length;
         if (length == 0) return [];
-    
+        
+        
         for (var i = 0; i < length; i++) {
           var anime = items[i];
           // Somehow, next line is parsed as well
           if (anime.isWhitespace) continue;
-    
+          // console.log(anime);
           var animeImage = anime.querySelector('.img');
           var animeLink = GoGoAnime.MainURL + animeImage.childNodes[1].attributes.href;
           var animeName = anime.querySelector('.name').text;
-          var animeEpisode = anime.querySelector('.episode').text;
+          // Only for NewRelease, it is displaying episode.
+          var extraInformation = this.url == GoGoAnime.NewRelease ? anime.querySelector('.episode').text : anime.querySelector('.released').removeWhitespace().text;
           var animeThumbnail = animeImage.childNodes[1].childNodes[1].attributes.src;
-          animeData.push({name: animeName, episode: animeEpisode, link: animeLink, thumbnail: animeThumbnail});
+          animeData.push({name: animeName, episode: extraInformation, link: animeLink, thumbnail: animeThumbnail});
         }
         // console.log(animeData);
         resolve(animeData);
