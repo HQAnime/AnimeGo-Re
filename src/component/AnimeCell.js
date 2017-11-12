8cf4241fa64a99f369a2bed4a28ffa45e8fc0dac
Yiheng <development.henryquan@gmail.com>
2017-11-12 16:10:07 +1100

    Fix layout issue

--- src/component/AnimeCell.js
+++ src/component/AnimeCell.js
@@ -11,13 +11,14 @@ class AnimeCell extends React.PureComponent {
     super(props);
     this.data = this.props.data;
     this.width = this.props.width;
+    this.flex = this.width / Dimensions.get('window').width;
     this.title = this.data.info.replace('Released: ', '');
   }
 
   render() {
     return (
       <Card image={{uri: this.data.thumbnail}} imageStyle={{height: this.width * 1.45}}
-        containerStyle={{padding: 0, margin: 0, width: this.width, justifyContent:'center'}}>
+        containerStyle={{padding: 0, margin: 0, flex: this.flex, justifyContent:'center'}}>
           <Text style={{marginBottom: 10, textAlign: 'center', height: 50}} numberOfLines={2}>{this.data.name}</Text>
           <Button backgroundColor={Colour.GoGoAnimeBlue} title={this.title}
             buttonStyle={{borderRadius: 15, height: 30, flex: 1}} onPress={this.buttonPressed}/>
