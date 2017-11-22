4a4f0e763e58762608032433e1d4a1c456f36ab0
Yiheng <development.henryquan@gmail.com>
2017-11-22 19:23:21 +0800

    Video needs to be parsed
    For VidStream and Estream, I could parse their link and play with vlc for android. For ios, you could use safari. However, for windows, I wonder if video could work for windows

--- android/app/src/main/java/com/yihengquan/MainApplication.java
+++ android/app/src/main/java/com/yihengquan/MainApplication.java
@@ -3,6 +3,7 @@ package com.yihengquan;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.brentvatne.react.ReactVideoPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,7 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage()
+            new ReactVideoPackage(),
+              new VectorIconsPackage()
       );
     }
 
--- src/screens/PlayAnime.js
+++ src/screens/PlayAnime.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
-import { WebView, Alert } from 'react-native';
-import { Actions } from 'react-native-router-flux'; 
+import { Alert, Linking } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+import Video from 'react-native-video'; 
+import { Colour } from '../Styles';
 
 class PlayAnime extends Component {
+  componentDidMount() {
+    this.player.presentFullscreenPlayer();    
+  }    
+
   render() {
     return (
-      <WebView source={{uri: this.props.link}} mediaPlaybackRequiresUserAction={false} onError={this.goBack}
-        allowsInlineMediaPlayback mixedContentMode='compatibility'/>
+      <Video source={{uri: 'https://bjdobr10.s.llnwi.net/v1/nguyen1/anime/171122/boruto34_HD.mp4'}}   // Can be a URL or a local file.
+       ref={(ref) => {
+         this.player = ref
+       }}                                      // Store reference
+       rate={1.0}                              // 0 is paused, 1 is normal.
+       volume={1.0}                            // 0 is muted, 1 is normal.
+       muted={false}                           // Mutes the audio entirely.
+       paused={false}                          // Pauses playback entirely.
+       resizeMode="cover"                      // Fill the whole screen at aspect ratio.*
+       repeat={false}                           // Repeat forever.
+       playInBackground={false}                // Audio continues to play when app entering background.
+       playWhenInactive={false}                // [iOS] Video continues to play when control or notification center are shown.
+       ignoreSilentSwitch={"ignore"}           // [iOS] ignore | obey - When 'ignore', audio will still play with the iOS hard silent switch set to silent. When 'obey', audio will toggle with the switch. When not specified, will inherit audio settings as usual.
+       onError={this.goBack}
+       style={styles.backgroundVideo}/>
     );
   }
 
@@ -15,5 +34,15 @@ class PlayAnime extends Component {
     Actions.pop();
   }
 }
+
+const styles = {
+  backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+};
 
 export { PlayAnime };
