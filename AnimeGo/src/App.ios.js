c1e3cce81e3033a5900826537556ec6692f65e1b
Yiheng <development.henryquan@gmail.com>
2018-02-19 12:16:27 +0800

    Remove old sketches
    A new logo will be created
    Anime GO

---
+++ AnimeGo/src/App.ios.js
@@ -0,0 +1,18 @@
+/*
+  App.js
+  Created on 19 Feb 2018 
+  by Yiheng Quan
+*/
+
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+
+      </View>
+    )
+  }
+}
