63ce5718ca0030932bb03ca4752fe72e41c417c4
Yiheng <development.henryquan@gmail.com>
2018-02-21 17:50:45 +0800

    Drawer for Android
    The colour is a bit off and I need to fixed it. Time to link all the screens. Anime Go remake is going well so far.

---
+++ AnimeGo/src/component/cell/DrawerCellStyle.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  textStyle: {
+    marginLeft: 16,
+    color: 'white',
+    fontSize: 18,
+  },
+  textViewStyle: {
+    height: 48,
+    justifyContent: 'center'
+  }
+})
