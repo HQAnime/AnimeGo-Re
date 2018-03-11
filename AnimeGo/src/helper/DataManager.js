b199a4b2b2fe1f58f1a95a013557a1c18b0f0552
Yiheng <development.henryquan@gmail.com>
2018-03-11 18:05:03 +1100

    More data entries
    For future updates

--- AnimeGo/src/helper/DataManager.js
+++ AnimeGo/src/helper/DataManager.js
@@ -1,4 +1,5 @@
 import { AsyncStorage } from 'react-native';
+import { VERSION } from '../value';
 
 class DataManager {
   static async setupData() {
@@ -10,17 +11,31 @@ class DataManager {
         await AsyncStorage.setItem('@FIRST', 'false');
         await AsyncStorage.setItem('@dataSaver', 'false');
         await AsyncStorage.setItem('@DUB', 'false');
+        await AsyncStorage.setItem('@Favourite', JSON.stringify({}));
+        await AsyncStorage.setItem('@Version', VERSION);
         global.dataSaver = false;
       } else {
         // Retrieve DATA
+        const version = await AsyncStorage.getItem('@Version');
+        if (version == null || version != VERSION) {
+          // Adding more stuff here
+          await DataManager.addMoreEntry();
+          // Update version number
+          AsyncStorage.setItem('@Version', VERSION);
+        }
         const dataSaver = await AsyncStorage.getItem('@dataSaver'); 
-        const DUB = await AsyncStorage.getItem('@DUB'); 
+        const DUB = await AsyncStorage.getItem('@DUB');
+        const favList = await AsyncStorage.getItem('@Favourite');  
         global.dataSaver = JSON.parse(dataSaver);
         global.hideDub = JSON.parse(DUB);
       }
     } catch (error) {
       console.error(error);
     }
+  }
+
+  static async addMoreEntry() {
+    await AsyncStorage.setItem('@Favourite', JSON.stringify({}));
   }
 }
 
