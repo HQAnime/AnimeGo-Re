7601290356fa081b72f2f023b29758a8e7283865
Yiheng <development.henryquan@gmail.com>
2018-03-25 17:16:44 +1100

    Schedule fixed
    Sunday bug and Image added
    
    
    Former-commit-id: d3499f3956e59e487b043e6e0d0731440ca7a140

--- AnimeGo/src/helper/core/AnimeSchedule.js
+++ AnimeGo/src/helper/core/AnimeSchedule.js
@@ -5,10 +5,10 @@ export default class AnimeSchedule {
   getScheduleForToday() {
     return new Promise((success, failure) => {
       // Loading data here
-      let weekdays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+      let weekdays = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
       fetch(MajorLink.Schedule).then((html) => html.text()).then((htmlText) => {
         var HTMLParser = require('fast-html-parser');
-        let weekdayClass = '.js-seasonal-anime-list-key-' + weekdays[new Date().getDay() - 1];
+        let weekdayClass = '.js-seasonal-anime-list-key-' + weekdays[new Date().getDay()];
         var root = HTMLParser.parse(htmlText).querySelector(weekdayClass);
         // Somehow, nothing is found
         if (root == null) success([]);
@@ -24,9 +24,10 @@ export default class AnimeSchedule {
           var link = ''; let linkPath = curr[0].childNodes[1].childNodes;
           if (linkPath[1].isWhitespace) link = linkPath[0].childNodes[1].attributes.href;
           else link = linkPath[1].childNodes[1].attributes.href;
+          let image = curr[2].childNodes[0].attributes["data-src"];
           let time = this.cleanText(curr[6].childNodes[1].text).split(' -')[1];
           let rating = '⭐️' + this.cleanText(curr[6].childNodes[3].childNodes[3].text);
-          animeSchedule.push({name: name, link: link, time: time, rating: rating});
+          animeSchedule.push({name: name, link: link, time: time, image: image, rating: rating});
         }
          console.log(animeSchedule);
         success(animeSchedule);
