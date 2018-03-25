291cb8134101cd2614598aea833fe931660ef0c1
Yiheng <development.henryquan@gmail.com>
2018-03-25 17:35:15 +1100

    Ready for UWP update
    Styles are fitting UWP and it is ready expect for icons
    
    
    Former-commit-id: acd37eea0f5f1fae06d6fc632e5343ee3eb2ff2f

--- AnimeGo/src/value.js
+++ AnimeGo/src/value.js
@@ -15,7 +15,7 @@ export const GreenColour = '#4CAF50';
 
 export const Github = 'https://github.com/HenryQuan/AnimeGo';
 export const LastestRelease = 'https://github.com/HenryQuan/AnimeGo/releases/latest';
-export const GoGoAnime = 'http://www.gogoanime.io';
+export const GoGoAnime = 'http://gogoanime.se';
 export const GooglePlay = 'https://play.google.com/store/apps/details?id=com.yihengquan.gogoanime';
 export const MicrosoftStore = 'https://www.microsoft.com/store/p/anime-go/9mx3qr26zfdj?rtc=1';
 export const Email = 'mailto:development.henryquan@gmail.com';
