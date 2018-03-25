4d801858c8efc783ee40018215729069700401e6
Yiheng <development.henryquan@gmail.com>
2018-03-25 18:21:41 +1100

    Remove admob
    COMPLETELY

--- AnimeGo/android/app/src/main/java/com/yihengquan/gogoanime/MainApplication.java
+++ AnimeGo/android/app/src/main/java/com/yihengquan/gogoanime/MainApplication.java
@@ -2,7 +2,8 @@ package com.yihengquan.gogoanime;
 
 import android.app.Application;
 
-import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactApplication;
+
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,7 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNAdMobPackage(),
             new VectorIconsPackage()
       );
     }
