a61e2068a48066f596422ac27ba10f1349aae070
Yiheng <development.henryquan@gmail.com>
2018-03-26 21:35:08 +1100

    Making package
    A great update is coming soon
    
    
    Former-commit-id: 405cbf0da8fcb7841b982d9f597097cdc00031d7

--- AnimeGo/windows/AnimeGo/Properties/AssemblyInfo.cs
+++ AnimeGo/windows/AnimeGo/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AnimeGo")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An unofficial application for gogoanime website")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Yiheng Quan")]
 [assembly: AssemblyProduct("AnimeGo")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Yiheng Quan  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.778.0")]
+[assembly: AssemblyFileVersion("1.0.778.0")]
 [assembly: ComVisible(false)]
