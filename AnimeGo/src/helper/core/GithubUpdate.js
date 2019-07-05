4bab2b81fd0e53e58dc055868afd0c5b6be2cc95
Yiheng (NUC) <development.henryquan@gmail.com>
2019-07-05 19:19:27 +1000

    update github update
    Former-commit-id: ab2ca6a908a29efc3da34fd88f8563b3a58e1933

--- AnimeGo/src/helper/core/GithubUpdate.js
+++ AnimeGo/src/helper/core/GithubUpdate.js
@@ -1,5 +1,5 @@
 import { Alert, Linking } from 'react-native';
-import { VERSION } from '../../value';
+import { VERSION, LastestRelease } from '../../value';
 
 export default class GithubUpdate {
 
@@ -9,28 +9,19 @@ export default class GithubUpdate {
 
   checkUpdate() {
     return new Promise((success, failure) => {
-      fetch(this.url).then((html) => html.text()).then((htmlText) => {
-        let HTMLParser = require('fast-html-parser');
-        let root = HTMLParser.parse(htmlText);
-        let newVersion = root.querySelector('.css-truncate-target');
-        let download = root.querySelector('.my-4');
-        // When Github is down or Henry being stupid
-        if (newVersion == null || download == null) Alert.alert('Fatal Error', 'APK not found');
-        // A new version
-        let code = newVersion.text;
+      fetch(this.url).then((html) => html.json()).then((json) => {
+        let code = json['version'];
         if (code > VERSION) {
-          let link = 'https://github.com/' + download.childNodes[3].childNodes[1].childNodes[1].attributes.href;
           Alert.alert('Update available', 'Do you want to download new apk?',
             [
               {text: 'Cancel', onPress: () => console.log('User cancelled update'), style: 'cancel'},
-              {text: 'OK', onPress: () => Linking.openURL(link)},
+              {text: 'OK', onPress: () => Linking.openURL(LastestRelease)},
             ],
           )
-        } else if (code < VERSION){
-          Alert.alert('はわわです', 'How could you get version ' + VERSION + '\nHave you hacked the version number?');
         } else{
           Alert.alert(code, 'AnimeGo is up to date');
         }
+        success();
       })
       .catch((error) => {
         Alert.alert('Fatal', 'Error Unknown');        
