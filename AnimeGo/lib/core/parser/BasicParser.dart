8576bfd79df8acf9ceef422cbef62daed651f031
Yiheng <development.henryquan@gmail.com>
2020-03-17 13:01:08 +1100

    copy paste from animeone and added http

--- AnimeGo/lib/core/parser/BasicParser.dart
+++ AnimeGo/lib/core/parser/BasicParser.dart
@@ -1,3 +1,42 @@
-class BasicParser {
+import 'dart:async';
+
+import 'package:html/dom.dart';
+import 'package:html/parser.dart';
+import 'package:http/http.dart' as http;
+
+/// This is the parent of all parsers and it handles 404 not found. 
+/// This is also the termination point of next page or back to home
+abstract class BasicParser {
+
+  String _link;
+  /// Get the link for current page
+  String getLink() => this._link;
+
+  BasicParser(String link) {
+    this._link = link;
+    print(this._link);
+  }
+
+  /// Download HTML string from link
+  Future<Document> downloadHTML() async {
+    try {
+      final response = await http.get(
+        this._link,
+      ).timeout(Duration(seconds: 10));
+
+      if (response.statusCode == 200) {
+        return parse(response.body);
+      } else {
+        // If it is 404, the status code will tell you
+        return null;
+      }
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  /// All subclasses have different implementations
+  parseHTML(Document body);
 
 }
