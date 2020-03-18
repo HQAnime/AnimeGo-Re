686986340f0e210c85f9baa52afa3d99b514922e
Yiheng <development.henryquan@gmail.com>
2020-03-18 18:32:36 +1100

    a better tablet view

--- AnimeGo/lib/ui/page/TabletHomePage.dart
+++ AnimeGo/lib/ui/page/TabletHomePage.dart
@@ -1,5 +1,7 @@
 import 'package:AnimeGo/ui/page/CategoryPage.dart';
 import 'package:AnimeGo/ui/widget/AnimeDrawer.dart';
+import 'package:AnimeGo/ui/widget/AnimeGrid.dart';
+import 'package:AnimeGo/ui/widget/GenreList.dart';
 import 'package:flutter/material.dart';
 
 /// TabletHomePage class
@@ -14,15 +16,61 @@ class TabletHomePage extends StatefulWidget {
 class _TabletHomePageState extends State<TabletHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AnimeGo'),
+      ),
+      body: Row(
+        children: <Widget>[
+          Flexible(
+            flex: 1,
+            child: renderMenu(),
+          ),
+          Flexible(
+            flex: 3,
+            child: AnimeGrid(url: '/page-recent-release.html'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  /// It is similar to `AnimeDrawer` yet different
+  Widget renderMenu() {
+    return ListView(
       children: <Widget>[
-        Flexible(
-          flex: 1,
-          child: AnimeDrawer(),
+        ListTile(
+          title: Text('Seasonal'),
+          leading: Icon(Icons.fiber_new),
         ),
-        Flexible(
-          flex: 3,
-          child: CategoryPage(title: 'Latest', url: '/page-recent-release.html',),
+        ListTile(
+          title: Text('Movie'),
+          leading: Icon(Icons.movie),
+        ),
+        ListTile(
+          title: Text('Popular'),
+          leading: Icon(Icons.label),
+        ),
+        ExpansionTile(
+          title: Text('Genre'),
+          leading: Icon(Icons.list),
+          children: <Widget>[
+            GenreList()
+          ],
+        ),
+        Divider(),
+        ListTile(
+          title: Text('History'),
+          leading: Icon(Icons.history),
+        ),
+        ListTile(
+          title: Text('Favourite'),
+          leading: Icon(Icons.favorite),
+        ),
+        Divider(),
+        ListTile(
+          title: Text('Settings'),
+          leading: Icon(Icons.settings),
         ),
       ],
     );
