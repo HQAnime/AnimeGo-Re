3a5a57f70a414125d81708fa05c178893d847128
Yiheng <development.henryquan@gmail.com>
2021-04-03 00:25:01 +1100

    removed the entire folder to rename correctly

--- AnimeGo/lib/core/model/FavouriteAnime.dart
+++
@@ -1,9 +0,0 @@
-import 'package:animego/core/model/BasicAnimeList.dart';
-
-class FavouriteAnime extends BasicAnimeList {
-  FavouriteAnime();
-  FavouriteAnime.fromJson(Map<String, dynamic> json) : super.fromJson(json);
-
-  @override
-  String getName() => 'favourite';
-}
