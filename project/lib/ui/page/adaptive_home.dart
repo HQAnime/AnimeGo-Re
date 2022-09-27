d758e060e25355a0356cf1a3c8073ad802dd29d9
Yiheng <development.henryquan@gmail.com>
2022-09-28 00:03:02 +1000

    load all pages based on selection

--- project/lib/ui/page/adaptive_home.dart
+++ project/lib/ui/page/adaptive_home.dart
@@ -1,3 +1,8 @@
+import 'package:animego/ui/page/Favourite.dart';
+import 'package:animego/ui/page/History.dart';
+import 'package:animego/ui/page/SeasonalAnime.dart';
+import 'package:animego/ui/page/Settings.dart';
+import 'package:animego/ui/widget/AnimeGrid.dart';
 import 'package:flutter/material.dart';
 
 class AdaptiveHomePage extends StatefulWidget {
@@ -76,6 +81,54 @@ const _navigationItems = [
 class _AdaptiveHomePageState extends State<AdaptiveHomePage> {
   _PageCode _selectedPage = _PageCode.latest;
 
+  // switch (code) {
+  //     case PageCode.latest:
+  //       return AnimeGrid(
+  //         url: '/page-recent-release.html',
+  //         key: Key(code.toString()),
+  //       );
+  //     case PageCode.seasonal:
+  //       return SeasonalAnime(embedded: true, key: Key(code.toString()));
+  //     case PageCode.movie:
+  //       return AnimeGrid(url: '/anime-movies.html', key: Key(code.toString()));
+  //     case PageCode.popular:
+  //       return AnimeGrid(url: '/popular.html', key: Key(code.toString()));
+  //     case PageCode.genre:
+  //       return AnimeGrid(url: genre, key: Key(genre));
+  //     case PageCode.history:
+  //       return const History(embedded: true);
+  //     case PageCode.favourite:
+  //       return const Favourite(embedded: true);
+  //     case PageCode.setting:
+  //       return const Settings(embedded: true);
+  //     default:
+  //       assert(false, 'Unknown page code');
+  //       return Container();
+  //   }
+
+  final _pages = <_PageCode, Widget>{
+    _PageCode.latest: AnimeGrid(
+      url: '/page-recent-release.html',
+      key: Key(_PageCode.latest.toString()),
+    ),
+    _PageCode.seasonal: SeasonalAnime(
+      embedded: true,
+      key: Key(_PageCode.seasonal.toString()),
+    ),
+    _PageCode.movie: AnimeGrid(
+      url: '/anime-movies.html',
+      key: Key(_PageCode.movie.toString()),
+    ),
+    _PageCode.popular: AnimeGrid(
+      url: '/popular.html',
+      key: Key(_PageCode.popular.toString()),
+    ),
+    _PageCode.genre: AnimeGrid(url: '/popular.html'),
+    _PageCode.history: const History(embedded: true),
+    _PageCode.favourite: const Favourite(embedded: true),
+    _PageCode.setting: const Settings(embedded: true),
+  };
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -90,8 +143,8 @@ class _AdaptiveHomePageState extends State<AdaptiveHomePage> {
             body: Row(
               children: [
                 _renderRail(),
-                Center(
-                  child: Text('AdaptiveHomePage'),
+                Expanded(
+                  child: _renderPage(),
                 ),
               ],
             ),
@@ -138,5 +191,10 @@ class _AdaptiveHomePageState extends State<AdaptiveHomePage> {
         ],
       ),
     );
+  }
+
+  Widget _renderPage() {
+    final page = _pages[_selectedPage];
+    return page ?? const Text('Page not found');
   }
 }
