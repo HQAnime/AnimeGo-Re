f916139e12336886cfe772d4965beaf0d99dc5be
Yiheng <development.henryquan@gmail.com>
2022-09-27 21:07:39 +1000

    render both drawer and rail

--- project/lib/ui/page/adaptive_home.dart
+++ project/lib/ui/page/adaptive_home.dart
@@ -20,15 +20,110 @@ enum _PageCode {
   setting,
 }
 
+/// A holder for both navigation rail and the drawer
+class _NavigationItem {
+  const _NavigationItem({
+    required this.code,
+    required this.title,
+    required this.icon,
+  });
+
+  final _PageCode code;
+  final String title;
+  final IconData icon;
+}
+
+const _navigationItems = [
+  _NavigationItem(
+    code: _PageCode.latest,
+    title: 'Latest',
+    icon: Icons.home,
+  ),
+  _NavigationItem(
+    code: _PageCode.seasonal,
+    title: 'Seasonal',
+    icon: Icons.calendar_today,
+  ),
+  _NavigationItem(
+    code: _PageCode.movie,
+    title: 'Movie',
+    icon: Icons.movie,
+  ),
+  _NavigationItem(
+      code: _PageCode.popular, title: 'Popular', icon: Icons.whatshot),
+  _NavigationItem(
+    code: _PageCode.genre,
+    title: 'Genre',
+    icon: Icons.category,
+  ),
+  _NavigationItem(
+    code: _PageCode.history,
+    title: 'History',
+    icon: Icons.history,
+  ),
+  _NavigationItem(
+    code: _PageCode.favourite,
+    title: 'Favourite',
+    icon: Icons.favorite,
+  ),
+  _NavigationItem(
+    code: _PageCode.setting,
+    title: 'Settings',
+    icon: Icons.settings,
+  ),
+];
+
 class _AdaptiveHomePageState extends State<AdaptiveHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('AnimeGo'),
-      ),
-      body: const Center(
-        child: Text('AdaptiveHomePage'),
+    return Row(
+      children: [
+        _renderDrawer(),
+        Expanded(
+          child: Scaffold(
+            appBar: AppBar(
+              title: const Text('AnimeGo'),
+            ),
+            drawer: _renderDrawer(),
+            body: Row(
+              children: [
+                _renderRail(),
+                Center(
+                  child: Text('AdaptiveHomePage'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  NavigationRail _renderRail() {
+    return NavigationRail(
+      labelType: NavigationRailLabelType.selected,
+      destinations: [
+        for (final item in _navigationItems)
+          NavigationRailDestination(
+            icon: Icon(item.icon),
+            label: Text(item.title),
+          ),
+      ],
+      selectedIndex: 0,
+    );
+  }
+
+  Drawer _renderDrawer() {
+    return Drawer(
+      child: ListView(
+        children: [
+          DrawerHeader(child: Text('Drawer Header')),
+          for (final item in _navigationItems)
+            ListTile(
+              leading: Icon(item.icon),
+              title: Text(item.title),
+            )
+        ],
       ),
     );
   }
