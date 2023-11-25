05ec575c2a0f3421f5fe006b649d7f0f95df1c0a
Yiheng <development.henryquan@gmail.com>
2023-11-25 11:15:18 +1100

    setup release signing

---
+++ project/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\libs\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\nateq\\Documents\\GitHub\\AnimeGo-Re\\project" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.3.1+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 3 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\libs\\flutter"
+  "PROJECT_DIR=C:\\Users\\nateq\\Documents\\GitHub\\AnimeGo-Re\\project"
+  "FLUTTER_ROOT=C:\\libs\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\nateq\\Documents\\GitHub\\AnimeGo-Re\\project\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\nateq\\Documents\\GitHub\\AnimeGo-Re\\project"
+  "FLUTTER_TARGET=C:\\Users\\nateq\\Documents\\GitHub\\AnimeGo-Re\\project\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC83NGQxNjYyN2I5NDBiYjE1ZTUwODkxZjgyY2FkNmMzZTM0NjViZDZkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\nateq\\Documents\\GitHub\\AnimeGo-Re\\project\\.dart_tool\\package_config.json"
+)
